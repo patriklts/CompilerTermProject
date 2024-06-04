c263f4c5378dbde05d1706fbd629b4ae0f1713c4
Nightsleeper2 <lukas.welker04@gmail.com>
2024-06-04 17:23:54 +0900

    implemented most of the reduce logic

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -17,28 +17,52 @@ def read_file_to_word_array(file_path: str) -> List[str]:
     terminals.extend("$")
     return terminals
 
+def get_lhs(derivation_rule):
+  """Extracts the left-hand side (LHS) of a derivation rule.
+
+  Args:
+      derivation_rule: The string representing the derivation rule.
+
+  Returns:
+      The left-hand side (non-terminal) of the rule, or None if no "->" is found.
+  """
+  parts = derivation_rule.split(" -> ")
+  return parts[0] if len(parts) > 0 else None
+
+def error_handling():
+    # TODO proper error handling
+    exit()
+
 def reduce(action: str, index: int):
-    # ToDo implement logic to reduce with derivations
-    print("reduce")
-    #ToDo goto handling
-    return -1
+    print(action)
+    stack.pop() # TODO pop so oft wie rechtse seite elemente hat
+    derivation_rule = int(action[1:])
+    nonTerminal_of_derivation_rule = get_lhs(derivations[derivation_rule])
+    if (nonTerminal_of_derivation_rule is None):
+        print("The derivation rule: ", derivation_rule, " does not exist" )
+        error_handling()
+    # TODO parse tree hier erstellen! mithilfe der derivation rule
+    current_state = stack.peek()
+    stack.push(goto_map[current_state][nonTerminal_of_derivation_rule])
     
 def shift(action: str, index: int):
+    print(action)
     stack.push(int(action[1:])) # push state on stack
-    return index + 1
+    return index + 1 # move the splitter to the right
 
 def check_tokens(tokens: List[str]):          
     index = 0
     
     while(True):
         # check if for the given state and the token there is something in the action map
-        state = stack.peek()
+        current_state = stack.peek()
+        next_input_symbol = tokens[index]
 
-        if not tokens[index] in action_map[state]:            
+        if not tokens[index] in action_map[current_state]:            
             print("error handling bitte")
             break
 
-        action = action_map[state][tokens[index]] 
+        action = action_map[current_state][next_input_symbol] 
 
         if action[0] == 'r':    # if action starts with r -> reduce
            reduce(action, index)
@@ -47,7 +71,7 @@ def check_tokens(tokens: List[str]):
             index = shift(action, index)
                 
         elif action == "acc":     # if action is acc -> accept
-            print("accept")     # ToDo: implement accept logic
+            print("accept")     # TODO: implement accept logic
             break
 
         print(index, stack.peek())
@@ -60,4 +84,5 @@ if __name__ == '__main__':
     input_file = sys.argv[1]
     stack = Stack()
     stack.push(0)   # push start state to stack
-    check_tokens(read_file_to_word_array(input_file))
+    tokens = read_file_to_word_array(input_file) #array with our non terminals and terminals
+    check_tokens(tokens)
